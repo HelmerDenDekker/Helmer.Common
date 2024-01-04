9fcbcb0e30a98ab7731439e49f9798e773502f3f
Helmer <helmer.dendekker@beterbed.net>
2024-01-04 14:55:58 +0100

    Tools init

---
+++ src/Helmer.Shared.Tools/Reactive/DistinctBehaviorSubject.cs
@@ -0,0 +1,29 @@
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Helmer.Shared.Tools.Reactive;
+
+/// <summary>
+/// Reactive extension on the BehaviourSubject, Distinct Until Changed
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class DistinctBehaviorSubject<T>
+{
+    protected DistinctBehaviorSubject(T init)
+    {
+        Subject = new BehaviorSubject<T>(init);
+    }
+
+    public T Value
+    {
+        get => Subject.Value;
+        set => Subject.OnNext(value);
+    }
+
+    public BehaviorSubject<T> Subject { get; private set; }
+
+    /// <summary>
+    /// When the Value changed
+    /// </summary>
+    public IObservable<T> ValueChanged => Subject.DistinctUntilChanged();
+}
