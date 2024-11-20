cfb8f9097284bb49b6e06bad7f39a9a55842560f
Helmer <helmer.dendekker@beterbed.net>
2024-11-20 09:17:48 +0100

    validator and tests

---
+++ src/Tests/Helmer.Shared.Tools.UnitTests/Validation/BaseValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using Helmer.Shared.Tools.UnitTests.TestData;
+using Helmer.Shared.Tools.Validation;
+
+namespace Helmer.Shared.Tools.UnitTests.Validation;
+
+public class BaseValidatorTests
+{
+	[Fact]
+	public void IsValid_WithValidModel_ReturnsTrue()
+	{
+		// Arrange
+		var validator = new BaseValidator<TestModel>();
+		var model = new TestModel
+		{
+			Name = "Test"
+		};
+
+		// Act
+		var result = validator.IsValid(model);
+
+		// Assert
+		Assert.True(result);
+	}
+}
